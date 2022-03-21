35a1620c689cd773173c215c14b09cd0e1bffe8b
Prakhar Gupta <30393146+iamprakhargupta@users.noreply.github.com>
2022-03-21 15:22:21 -0400

    Lab 2 FOL logic

---
+++ constant.py
@@ -0,0 +1,17 @@
+"""
+
+CSCI-630: Foundation of AI
+lab2 Code - constant.py
+Author: Prakhar Gupta
+Username: pg9349
+
+"""
+
+class Constant:
+    def __init__(self,load,neg):
+        self.load=load
+        self.neg=neg
+
+    def value(self):
+        return self.load
+
